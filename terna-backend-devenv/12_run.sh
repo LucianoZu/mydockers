f9912d93bb3ded0af245d3f9d77bce050b426375
Luciano Zu <luciano.zu@gmail.com>
2018-10-19 08:59:10 +0100

    Aggiunta del TimeZone (variabile d'ambiente TZ) per permettere al JDK di determinare la timezone esatta.

--- terna-backend-devenv/12_run.sh
+++ terna-backend-devenv/12_run.sh
@@ -9,5 +9,6 @@ docker run -dit -p 7001:7001 \
 -v /etc/localtime:/etc/localtime:ro \
 --device=/dev/dri/card0:/dev/dri/card0:rw \
 -e DISPLAY=:0 \
+-e TZ=Europe/Rome \
 lucianozu/terna-backend-devenv
 
