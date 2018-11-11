9e176389054a9b91254efba5e94a298fcb68f2a9
Luciano Zu <luciano.zu@gmail.com>
2018-11-11 17:25:51 +0100

    bug fix

--- terna-backend-devenv/12_run.sh
+++ terna-backend-devenv/12_run.sh
@@ -4,7 +4,7 @@ docker run -dit -p 7001:7001 \
 --name terna-backend-devenv -h terna-backend-devenv \
 -v terna-backend-devenv_volume:/home/weblogic/workspaces \
 -v terna-backend-devenv_volume_domains:/home/weblogic/wls12210/user_projects \
--v $HOME/.m2/repository:/root/.m2/repository:rw \
+-v $HOME/.m2/repository:/home/weblogic/.m2/repository:rw \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -v /etc/localtime:/etc/localtime:ro \
 -v /etc/timezone:/etc/timezone:ro \
